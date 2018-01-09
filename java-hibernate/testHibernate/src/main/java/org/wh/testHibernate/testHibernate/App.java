e0624779750dc9348fe03e717d861d566000d8e8
williams <willyneverdie@gmail.com>
2018-01-08 21:31:51 -0500

    test hibernate

---
+++ java-hibernate/testHibernate/src/main/java/org/wh/testHibernate/testHibernate/App.java
@@ -0,0 +1,13 @@
+package org.wh.testHibernate.testHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
